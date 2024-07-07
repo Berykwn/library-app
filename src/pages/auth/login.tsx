a6a28e2bb4dcb6c866f0a1fb72ddf41e27458b14
Berry Kurniawan <berykurniawan46@gmail.com>
2024-07-07 23:01:41 +0700

    book pages and show

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -11,6 +11,7 @@ import { Input } from "../../components/ui/input";
 import { Label } from "../../components/ui/label";
 import { Button } from "../../components/ui/button";
 
+
 const Login = () => {
   return (
     <Layout centerX centerY>
