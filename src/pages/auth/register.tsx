8d82bf0c33d52eb7b30881436a48fff479c50696
Berry Kurniawan <berykurniawan46@gmail.com>
2024-07-07 23:05:11 +0700

    book pages and show

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -10,6 +10,7 @@ import {
 import { Input } from "../../components/ui/input";
 import { Label } from "../../components/ui/label";
 import { Button } from "../../components/ui/button";
+// import { Button } from "../../components/ui/button";
 
 const Register = () => {
   return (
