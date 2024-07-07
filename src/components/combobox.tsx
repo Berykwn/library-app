b35051a0fa586a43e9c64924588d4522ed17339d
Berry Kurniawan <berykurniawan46@gmail.com>
2024-07-07 22:36:19 +0700

    book pages and show

--- src/components/combobox.tsx
+++ src/components/combobox.tsx
@@ -16,7 +16,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "../components/ui/popover";
-import { Button } from "../components/ui/button"
+import { Button } from "../components/ui/button";
 
 const frameworks = [
   {
